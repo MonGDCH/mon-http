63ebee54fa875146626ef07aca60fc5df7d91f24
mon <mon@mon.com>
2022-11-10 13:33:03 +0800

    优化配置

--- src/process/config/app.php
+++ src/process/config/app.php
@@ -5,4 +5,11 @@ return [
     'exception' => \support\http\HttpErrorHandler::class,
     // 是否每次业务重新创建控制器
     'newCtrl'   => true,
+    // 防火墙中间件
+    'firewall'  => [
+        // IP黑名单
+        'black' => [],
+        // IP白名单
+        'white' => []
+    ]
 ];
